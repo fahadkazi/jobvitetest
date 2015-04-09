d0420c40dd128909caebbbeb62206e3f2e6c1cf3
fahadkz <fhkz2005@gmail.com>
2015-04-09 10:14:38 +0530

    added more files

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
-var jobviteapp = angular.module('jobviteapp', []);
+var jobviteapp = angular.module('jobviteapp', ['ui.bootstrap']);
 
 jobviteapp.controller('jobviteController', ['$scope', '$http', function($scope, $http){
+
+	$scope.groups = [
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'Dynamic Group Body - 1'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'Dynamic Group Body - 2'
+    }
+  ];
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
 	
 }])
